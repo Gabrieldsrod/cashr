56bc0374ed75b8e89f501d481bb8398bcc94af15
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-05-14 22:06:26 -0300

    feat: garante encapsulamento na classe Account para evitar mudança de valor de campos

--- backend/src/main/java/com/gabrieldsrod/cashr/api/model/Account.java
+++ backend/src/main/java/com/gabrieldsrod/cashr/api/model/Account.java
@@ -1,10 +1,12 @@
 package com.gabrieldsrod.cashr.api.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -19,14 +21,17 @@ public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
     private UUID id;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, precision = 19, scale = 2)
+    @Setter(AccessLevel.NONE)
+    @Column(nullable = false, precision = 19, scale = 2, updatable = false)
     private BigDecimal initialBalance;
 
+    @Setter(AccessLevel.NONE)
     @Column(nullable = false)
     private UUID userId;
 
@@ -34,6 +39,7 @@ public class Account {
     @Column(nullable = false)
     private AccountType type;
 
+    @Setter(AccessLevel.NONE)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Currency currency;
